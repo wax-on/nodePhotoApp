7400691cd53d02dc3a09e656762e1c5313be63c5
Hugo Johansson <hugo.johansson@hyperisland.se>
2020-06-01 21:35:49 +0200

    Minor changes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../controllers/middlewares/auth");
+const auth = require("../controllers/middlewears/auth");
 
 //GET - Welcome
 router.get("/", (req, res) => {
