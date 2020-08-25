f28719b7659bf7369f2a24f90b4255c73025fe01
Hugo Johansson <hugo.johansson@hyperisland.se>
2020-08-25 11:38:29 +0200

    minor changes in code in photo_controller

--- controllers/photo_controller.js
+++ controllers/photo_controller.js
@@ -62,37 +62,6 @@ const getSpecPhoto = async (req, res) => {
   }
 };
 
-// POST /albums - Post to authenticated user's albums
-const postAlbums = async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    res.status(422).send({
-      status: "fail",
-      data: errors.array(),
-    });
-    return;
-  }
-  const validData = matchedData(req);
-
-  try {
-    const album = await new Album(validData).save({
-      user_id: req.user.attributes.id,
-    });
-    res.send({
-      status: "success",
-      data: {
-        album,
-      },
-    });
-  } catch (error) {
-    res.status(500).send({
-      status: "error",
-      message: "Exception thrown in database.",
-    });
-    throw error;
-  }
-};
-
 // POST /photos - Post to authenticated user's photos
 const postPhotos = async (req, res) => {
   const errors = validationResult(req);
